5cdfdf3e3c1e629e32efdd6a61d1f22c5de14e2c
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-09-15 11:21:04 +0300

    Update Main.java

--- test1/src/ru/vsu/cs/mvi/Main.java
+++ test1/src/ru/vsu/cs/mvi/Main.java
@@ -4,16 +4,25 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    public static double calculations(double radiusSmall, double radiusLarge){
+        double squareSmallCircle = Math.round(Math.pow(radiusSmall, 2) * Math.PI);   //Находим площадь маленького круга
+        double squareLargeCircle = Math.round(Math.pow(radiusLarge, 2) * Math.PI);   //Находим площадь большого круга
+        double squareArc = (squareLargeCircle - squareSmallCircle) / 4;   // Находим площадь дуги
+        double squareQuadrature = Math.pow(radiusLarge, 2);   // Находим площадь квадрата
+
+        return(Math.round((squareQuadrature - squareArc) + squareArc * 3));
+    }
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите радиус малой окружности");
+
+        System.out.println("Enter the radius of the small circle");
         double radiusSmall = scanner.nextDouble();
-        System.out.println("Введите радиус большой окружности");
+
+        System.out.println("Enter the radius of the great circle");
         double radiusLarge = scanner.nextDouble();
-        double squareSmallCircle = Math.pow(radiusSmall, 2) * Math.PI;
-        double squareLargeCircle = Math.pow(radiusLarge, 2) * Math.PI;
-        double squareResult = ((squareLargeCircle - squareSmallCircle) / 4) * 3 + Math.pow(radiusLarge, 2);
-        System.out.println(squareResult);
+
+        double result = calculations(radiusSmall, radiusLarge);
+        System.out.println(result);
     }
 }
