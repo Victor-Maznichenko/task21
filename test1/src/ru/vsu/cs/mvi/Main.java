c20e2475909797ab51d00a548581331fa7458856
Victor Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-09-29 11:23:51 +0300

    Update Main.java

--- test1/src/ru/vsu/cs/mvi/Main.java
+++ test1/src/ru/vsu/cs/mvi/Main.java
@@ -1,28 +1,34 @@
 package ru.vsu.cs.mvi;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static double calculations(double radiusSmall, double radiusLarge){
-        double squareSmallCircle = Math.round(Math.pow(radiusSmall, 2) * Math.PI);   //Находим площадь маленького круга
-        double squareLargeCircle = Math.round(Math.pow(radiusLarge, 2) * Math.PI);   //Находим площадь большого круга
-        double squareArc = (squareLargeCircle - squareSmallCircle) / 4;   // Находим площадь дуги
-        double squareQuadrature = Math.pow(radiusLarge, 2);   // Находим площадь квадрата
 
-        return(Math.round((squareQuadrature - squareArc) + squareArc * 3));
+    public static int LeonardoNumbersInRange(int a, int b) {
+        int tempValue;
+        int previousValue = 1;
+        int nextValue = 1;
+        int count = 1;
+        while(previousValue <= b){
+            tempValue = nextValue;
+            nextValue = previousValue + nextValue + 1;
+            previousValue = tempValue;
+            System.out.println(nextValue);
+            if(nextValue >= a){
+                count++;
+            }
+        }
+        return count;
     }
+
     public static void main(String[] args) {
-        Locale.setDefault(Locale.ROOT);
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число a:");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b:");
+        int b = scanner.nextInt();
 
-        System.out.println("Enter the radius of the small circle");
-        double radiusSmall = scanner.nextDouble();
-
-        System.out.println("Enter the radius of the great circle");
-        double radiusLarge = scanner.nextDouble();
-
-        double result = calculations(radiusSmall, radiusLarge);
-        System.out.println(result);
+        System.out.println("RESULT: " + LeonardoNumbersInRange(a, b));
     }
 }
+
