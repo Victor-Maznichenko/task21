cbeb30f6fde7ccb66fb86bc831425a3e31c09ea0
Victor Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-10-24 02:34:43 +0300

    Update Main.java

--- test1/src/ru/vsu/cs/mvi/Main.java
+++ test1/src/ru/vsu/cs/mvi/Main.java
@@ -1,35 +1,44 @@
-package ru.vsu.cs.mvi;
-
 import java.util.Scanner;
 
 public class Main {
+    public static void printStarTriangle(int s) {
+        int halfS = s / 2;
 
-    public static int LeonardoNumbersInRange(int a, int b) {
-        int tempValue;
-        int previousValue = 1;
-        int nextValue = 1;
-        int count = a == 1 ? 1 : 0;
-
-        while(nextValue <= b){
-            tempValue = nextValue;
-            nextValue = previousValue + nextValue + 1;
-            previousValue = tempValue;
-            System.out.println(previousValue + "    " + nextValue);
-            if(previousValue >= a){
-                count++;
+        for (int i = 0; i < halfS; i++) {
+            for(int j = 0; j < halfS; ++j) {
+                System.out.print(' ');
             }
+            for (double j = 0; j < (halfS - i); j++) {
+                double countJ = halfS - i;
+                if ((countJ == halfS) || (Math.ceil(countJ / 2) < 2) || (j == 0) || (j == countJ - 1)){
+                    System.out.print("*");
+                } else {
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
         }
-        return count;
+
+        for(double i = 1; i <= halfS; ++i) {
+            for(int g = 1; g <= (halfS - i); ++g) {
+                System.out.print(' ');
+            }
+            for(int j = 1; j <= i; ++j) {
+                if ((i == halfS) || (Math.ceil(i / 2) < 2) || (j == 1) || (j == i)){
+                    System.out.print("*");
+                } else {
+                    System.out.print("#");
+                }
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
+        System.out.println("Введите четное число s:");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int a = scanner.nextInt();
-        System.out.println("Enter b number:");
-        int b = scanner.nextInt();
-
-        System.out.println("RESULT: " + LeonardoNumbersInRange(a, b));
+        int s = scanner.nextInt();
+        printStarTriangle(s);
     }
 }
-
